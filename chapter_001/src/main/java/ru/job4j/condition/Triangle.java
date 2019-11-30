8d71968a71d2e4f18ccef8d7e05326f67d508e23
avtokot <avtokot84@gmail.com>
2019-11-30 12:08:45 +0200

    refactoring

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -2,10 +2,6 @@ package ru.job4j.condition;
 
 public class Triangle {
     public static boolean exist(double ab, double bc, double ac) {
-        boolean result = false;
-        if ((ab + bc) > ac && (bc + ac) > ab && (ac + ab) > bc) {
-            result = true;
-        }
-        return result;
+        return ((ab + bc) > ac && (bc + ac) > ab && (ac + ab) > bc);
     }
 }
