5e4bfc66a5f717cf26418519b6eeb7112f7033d2
avtokot <avtokot84@gmail.com>
2019-12-03 21:49:33 +0200

    CheckPrime

--- chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -7,16 +7,24 @@ import static org.junit.Assert.assertThat;
 
 public class CheckPrimeNumberTest {
     @Test
-    public void whenCheckPrimeNumberIsYes() {
+    public void whenCheckPrimeNumberThanFifeTrue() {
         CheckPrimeNumber checkPrimeNumber = new CheckPrimeNumber();
         boolean result = checkPrimeNumber.check(5);
         assertThat(result, is(true));
     }
 
     @Test
-    public void whenCheckPrimeNumberIsNow() {
+    public void whenCheckPrimeNumberThanFourNo() {
         CheckPrimeNumber checkPrimeNumber = new CheckPrimeNumber();
         boolean result = checkPrimeNumber.check(4);
         assertThat(result, is(false));
     }
+
+    @Test
+    public void whenCheckPrimeNumberThanNineNo() {
+        CheckPrimeNumber checkPrimeNumber = new CheckPrimeNumber();
+        boolean result = checkPrimeNumber.check(9);
+        assertThat(result, is(false));
+    }
 }
+
