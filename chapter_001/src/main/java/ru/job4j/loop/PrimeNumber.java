5c6c868f23a6ad9c7db0b02e6f8e09694efc3f8f
avtokot <avtokot84@gmail.com>
2019-12-05 09:09:22 +0200

    fix_prime_number

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -2,24 +2,11 @@ package ru.job4j.loop;
 
 public class PrimeNumber {
     public int calc(int num) {
+        CheckPrimeNumber prime = new CheckPrimeNumber();
         int count = 0;
         for (int i = 2; i <= num; i++) {
-            boolean prime = true;
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    prime = false;
-                    break;
-                }
-            }
-            if (prime) {
-                count++;
-            }
+            if (prime.check(i)) count++;
         }
         return count;
-    }
-
-    public static void main(String[] args) {
-        PrimeNumber primeNumber = new PrimeNumber();
-        System.out.println(primeNumber.calc(11));
     }
 }
