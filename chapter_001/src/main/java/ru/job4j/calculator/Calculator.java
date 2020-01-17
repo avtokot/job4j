6f633bdcaa5e0472719f47cad9d44dfc72235298
avtokot <avtokot84@gmail.com>
2020-01-17 19:39:52 +0200

    JavaDoc

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculator;
 
+/**
+ * Class for arithmetic operations
+ *
+ * @author Constantine
+ * @version 1
+ * @since 17.01.2020
+ */
 public class Calculator {
+    /**
+     * Method main implements arithmetic operations
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
@@ -8,21 +18,45 @@ public class Calculator {
         subtract(10, 5);
     }
 
+    /**
+     * Subtraction
+     *
+     * @param first  - first argument
+     * @param second - second argument
+     */
     private static void subtract(double first, double second) {
         double result = first - second;
         System.out.println(first + " - " + second + " = " + result);
     }
 
+    /**
+     * Multiply
+     *
+     * @param first  - first argument
+     * @param second - second argument
+     */
     private static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + " * " + second + " = " + result);
     }
 
+    /**
+     * Divide
+     *
+     * @param first  - first argument
+     * @param second - second argument
+     */
     private static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + " / " + second + " = " + result);
     }
 
+    /**
+     * Adding
+     *
+     * @param first  - first argument
+     * @param second - second argument
+     */
     private static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + " + " + second + " = " + result);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Constantine
+ * @version $id$
+ * @since 0.1
+ */
+package ru.job4j.calculator;
