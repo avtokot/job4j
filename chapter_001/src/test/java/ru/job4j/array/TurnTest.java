6eb117f7cc62dd13292b7c496cde5ba88fc0ff8c
avtokot <avtokot84@gmail.com>
2020-01-20 22:24:29 +0200

    Turn

---
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import ru.job4j.array.Turn;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TurnTest {
+    @Test
+    public void whenArrayTurnBack() {
+        Turn turn = new Turn();
+        int[] array = {1, 2, 3, 4};
+        int[] result = turn.back(array);
+        int[] expect = {4, 3, 2, 1};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTurnArrayWithOddOfItems() {
+        Turn turn = new Turn();
+        int[] array = {1, 2, 3, 4, 5};
+        int[] result = turn.back(array);
+        int[] expect = {5, 4, 3, 2, 1};
+        assertThat(result, is(expect));
+    }
+}
