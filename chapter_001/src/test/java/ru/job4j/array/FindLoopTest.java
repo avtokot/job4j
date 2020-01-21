960aa06894be4b16a06723cab862da84308339ed
avtokot <avtokot84@gmail.com>
2020-01-21 22:28:53 +0200

    FindLoopInIndex

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -15,4 +15,23 @@ public class FindLoopTest {
         int expect = 1;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenArrayFind3() {
+        FindLoop findLoop = new FindLoop();
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int[] arr = {5, 2, 10, 2, 4};
+        int result = findLoop.indexOf(arr, el, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayNotFoundElement() {
+        int result = -1;
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }
