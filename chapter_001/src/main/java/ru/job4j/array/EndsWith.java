ec150327bbf47d988ab7dae5a1b90a4bfb10ee74
avtokot <avtokot84@gmail.com>
2020-01-22 09:30:41 +0200

    EndsWith

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -4,12 +4,8 @@ public class EndsWith {
     public boolean endsWith(char[] word, char[] post) {
         boolean result = false;
         for (int i = word.length - 1, j = post.length - 1; j >= 0; i--, j--) {
-            if (word[i] == post[j]) {
-                result = true;
-            } else {
-                result = false;
-                break;
-            }
+            result = word[i] == post[j] ? true : false;
+            break;
         }
         return result;
     }
