f06ba4e3ade2477e3670e448825b689bcce3601d
avtokot <avtokot84@gmail.com>
2020-01-24 18:04:21 +0200

    Min

---
+++ chapter_001/src/main/java/ru/job4j/array/Min.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+
+public class Min {
+    public int findMin(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MinTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MinTest {
+    @Test
+    public void when3Min() {
+        Min min = new Min();
+        int[] arr = {4, 6, 3};
+        int result = min.findMin(arr);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when4Min() {
+        Min min = new Min();
+        int[] arr = {4, 7, 9};
+        int result = min.findMin(arr);
+        int expect = 4;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when2Min() {
+        Min min = new Min();
+        int[] arr = {6, 2, 9};
+        int result = min.findMin(arr);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+}
