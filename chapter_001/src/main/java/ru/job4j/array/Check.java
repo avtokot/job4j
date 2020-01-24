7f698030145921f0d21551ab129d62aae442a0fc
avtokot <avtokot84@gmail.com>
2020-01-24 15:54:07 +0200

    CheckFix

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -4,7 +4,10 @@ public class Check {
     public boolean mono(boolean[] data) {
         boolean result = true;
         for (int i = 0; i < data.length; i++) {
-            result = data[0] == data[i] ? true : false;
+            if (data[0] != data[i]) {
+                result = false;
+                break;
+            }
         }
         return result;
     }
