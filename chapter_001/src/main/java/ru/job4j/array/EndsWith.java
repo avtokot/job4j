4faedf2968773811027ea9a1f37417cc29ca21fa
avtokot <avtokot84@gmail.com>
2020-01-24 15:56:10 +0200

    EndaWithFix

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -2,10 +2,13 @@ package ru.job4j.array;
 
 public class EndsWith {
     public boolean endsWith(char[] word, char[] post) {
-        boolean result = false;
+        boolean result = true;
         for (int i = word.length - 1, j = post.length - 1; j >= 0; i--, j--) {
-            result = word[i] == post[j] ? true : false;
-            break;
+            if (word[i] != post[j]) {
+                result = false;
+                break;
+            }
+
         }
         return result;
     }
