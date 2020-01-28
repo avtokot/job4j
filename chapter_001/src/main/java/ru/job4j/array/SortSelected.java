69b659769b73db10b9b9a7eeb18f3c1da39b457f
avtokot <avtokot84@gmail.com>
2020-01-28 19:55:57 +0200

    SortSelected

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -1,12 +1,11 @@
 package ru.job4j.array;
 
-import java.util.Arrays;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, 0, data.length);
-            int index = FindLoop.indexOf(data, min, 0, data.length);
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexOf(data, min, i, data.length);
             if (data[i] > min) {
                 int temp = data[i];
                 data[i] = data[index];
--- chapter_001/src/test/java/ru/job4j/array/SortSelectedTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -7,10 +7,18 @@ import static org.junit.Assert.assertThat;
 
 public class SortSelectedTest {
     @Test
-    public void whenSort() {
-        int[] arr = {3, 5, 1, 7, 8};
+    public void whenSort5Numbers() {
+        int[] arr = {2, 4, 1, 5, 3};
         int[] result = SortSelected.sort(arr);
-        int[] expect = {1, 3, 5, 7, 8};
+        int[] expect = {1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort3Numbers() {
+        int[] arr = {4, 1, 9};
+        int[] result = SortSelected.sort(arr);
+        int[] expect = {1, 4, 9};
         assertThat(result, is(expect));
     }
 }
