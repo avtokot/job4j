b86d80e9cf4f1d13624b545a9277616e2493da03
avtokot <avtokot84@gmail.com>
2020-01-28 20:41:37 +0200

    MinDiapasonFix

--- chapter_001/src/main/java/ru/job4j/array/MinDiapason.java
+++ chapter_001/src/main/java/ru/job4j/array/MinDiapason.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 
 public class MinDiapason {
-    public  int findMin(int[] arr, int start, int finish) {
+    public static int findMin(int[] arr, int start, int finish) {
         int min = arr[start];
         for (int i = start; i <= finish; i++) {
             if (min > arr[i]) {
