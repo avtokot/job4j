e3e0b9cd642e05176f5b048c819a3cd5ff931e8c
avtokot <avtokot84@gmail.com>
2020-01-28 20:41:49 +0200

    MinDiapasonFix

--- chapter_001/src/test/java/ru/job4j/array/MinDiapasonTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MinDiapasonTest.java
@@ -8,32 +8,31 @@ import static org.junit.Assert.assertThat;
 public class MinDiapasonTest {
     @Test
     public void whenFirstMinDiapason() {
-        MinDiapason minDiapason = new MinDiapason();
         int[] arr = {-1, 0, 5, 10};
         int start = 1;
         int finish = 3;
-        int result = minDiapason.findMin(arr, start, finish);
+        int result = MinDiapason.findMin(arr, start, finish);
         int expect = 0;
         assertThat(result, is(expect));
     }
+
     @Test
     public void whenLastMinDiapason() {
-        MinDiapason minDiapason = new MinDiapason();
-        int[] arr = {-1, 10, 5, 1};
+        int[] arr = {10, 5, 3, 1};
         int start = 1;
         int finish = 3;
-        int result = minDiapason.findMin(arr, start, finish);
+        int result = MinDiapason.findMin(arr, start, finish);
         int expect = 1;
         assertThat(result, is(expect));
     }
+
     @Test
     public void whenMiddleMinDiapason() {
-        MinDiapason minDiapason = new MinDiapason();
-        int[] arr = {1, 11, 5, 10};
-        int start = 1;
-        int finish = 3;
-        int result = minDiapason.findMin(arr, start, finish);
-        int expect = 5;
+        int[] arr = {10, 2, 5, 1};
+        int start = 0;
+        int finish = 2;
+        int result = MinDiapason.findMin(arr, start, finish);
+        int expect = 2;
         assertThat(result, is(expect));
     }
 }
