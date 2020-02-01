3d04785adde26be77624aac792e95f8fbe5bd30a
avtokot <avtokot84@gmail.com>
2020-02-01 12:05:21 +0200

    BarleyBreakFix

--- chapter_001/src/main/java/ru/job4j/array/BarleyBreak.java
+++ chapter_001/src/main/java/ru/job4j/array/BarleyBreak.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class BarleyBreak {
     public static void main(String[] args) {
         int[][] arr = new int[3][3];
-        int k = 0;
+        int k = 1;
         for (int row = 0; row < arr.length; row++) {
             for (int col = 0; col < arr.length; col++) {
                 arr[row][col] = k;
