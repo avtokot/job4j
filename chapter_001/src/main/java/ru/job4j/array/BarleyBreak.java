089c13df5bb171937801c7895f86b8383acd10dd
avtokot <avtokot84@gmail.com>
2020-02-01 11:55:44 +0200

    BarleyBreak

---
+++ chapter_001/src/main/java/ru/job4j/array/BarleyBreak.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class BarleyBreak {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+        int k = 0;
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr.length; col++) {
+                arr[row][col] = k;
+                k++;
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
