16dde4c3e5388b6bd8975ae58bd99e1125c02246
avtokot <avtokot84@gmail.com>
2020-02-05 21:23:48 +0200

    MatrixCheckRow

---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckRowTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixCheckRowTest {
+    @Test
+    public void whenHasMonoHorizontalTrue() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '}
+        };
+        boolean result = MatrixCheckRow.monoHorizontal(input, 1);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasMonoHorizontalFalse() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', ' '},
+                {' ', ' ', 'X'}
+        };
+        boolean result = MatrixCheckRow.monoHorizontal(input, 1);
+        assertThat(result, is(false));
+    }
+}
