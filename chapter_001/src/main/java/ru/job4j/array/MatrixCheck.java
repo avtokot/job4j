a20b3b15ac70cf32fa43c0d415701c83ffac04f4
avtokot <avtokot84@gmail.com>
2020-02-06 23:22:53 +0200

    Diagonal

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,22 +1,55 @@
 package ru.job4j.array;
 
 public class MatrixCheck {
-    public static boolean isWin(char[][] board) {
-        boolean result = false;
-        int countRow = 0;
-        int countCol = 0;
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board.length; col++) {
-                if ((board[row][row] == 'X') || (board[col][col] == 'X')) {
-                    countRow++;
-                    countCol++;
-                    result = true;
-                }
+//    public static boolean isWin(char[][] board) {
+//        boolean result = false;
+//        int countRow = 0;
+//        int countCol = 0;
+//        for (int row = 0; row < board.length; row++) {
+//            for (int col = 0; col < board.length; col++) {
+//                if ((board[row][row] == 'X') || (board[col][col] == 'X')) {
+//                    countRow++;
+//                    countCol++;
+//                    result = true;
+//                }
+//                if (countCol == board.length || countRow == board.length) {
+//                    result = true;
+//                    break;
+//                }
+//            }
+//        }
+//        return result;
+//    }
+
+
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 'X') {
+                result = false;
+                break;
             }
         }
-        if (countCol == board.length || countRow == board.length) {
-            result = true;
+        return result;
+    }
+
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static char[] diagonal(char[][] board) {
+        char[] result = new char[board.length];
+        for (int i = 0; i < board.length; i++) {
+            result[i] = board[i][i];
         }
         return result;
     }
 }
+
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -9,39 +9,106 @@ public class MatrixCheckTest {
     @Test
     public void whenMatrixCheckTrueThenFalse() {
         char[][] input = {
+                {' ', 'X', ' ', ' ', ' '},
+                {' ', 'X', ' ', ' ', ' '},
                 {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', ' ', 'X', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '}
+                {' ', 'X', ' ', ' ', ' '},
+                {' ', 'X', ' ', ' ', ' '}
         };
-        boolean result = MatrixCheck.isWin(input);
-        assertThat(result, is(false));
+//        boolean result = MatrixCheck.isWin(input);
+//        assertThat(result, is(false));
     }
 
     @Test
     public void whenMatrixCheckTrueThenTrue() {
         char[][] input = {
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '}
+                {' ', 'X', ' ', ' ', ' '},
+                {' ', 'X', ' ', ' ', ' '},
+                {' ', 'X', ' ', ' ', ' '},
+                {' ', 'X', ' ', ' ', ' '},
+                {' ', 'X', ' ', ' ', ' '}
         };
-        boolean result = MatrixCheck.isWin(input);
-        assertThat(result, is(true));
+//        boolean result = MatrixCheck.isWin(input);
+//        assertThat(result, is(true));
     }
 
     @Test
     public void whenDataMonoTrueThenTrue() {
         char[][] input = {
                 {' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' '},
                 {'X', 'X', 'X', 'X', 'X'},
+                {' ', ' ', 'X', ' ', ' '},
                 {' ', ' ', 'X', ' ', ' '},
                 {' ', ' ', 'X', ' ', ' '}
         };
-        boolean result = MatrixCheck.isWin(input);
+//        boolean result = MatrixCheck.isWin(input);
+//        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenMonoVerticalTrue() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'}
+        };
+        boolean result = MatrixCheck.monoVertical(input, 2);
         assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenMonoVerticalFalse() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'}
+        };
+        boolean result = MatrixCheck.monoVertical(input, 2);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenHasMonoHorizontalTrue() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '}
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 1);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasMonoHorizontalFalse() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', ' '},
+                {' ', ' ', 'X'}
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 1);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDiagonalTrue() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'}
+        };
+        char[] result = MatrixCheck.diagonal(input);
+        char[] expect = {'X', 'X', 'X'};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenDiagonalFalse() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'}
+        };
+        char[] result = MatrixCheck.diagonal(input);
+        char[] expect = {'X', ' ', 'X'};
+        assertThat(result, is(expect));
     }
 }
