6efe27d595e4df0ddd0b126541331e7d573fa5d4
avtokot <avtokot84@gmail.com>
2020-02-28 18:27:27 +0200

    Class Dog

---
+++ chapter_001/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog rokki = new Dog();
+        Dog rick = new Dog();
+        Dog rock = new Dog();
+    }
+}
