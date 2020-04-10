f6ffeb2baeb88d3b8dc61b48c4f7b1e22acbec4a
avtokot <avtokot84@gmail.com>
2020-04-10 21:28:36 +0300

    Refactor Max

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -4,4 +4,16 @@ public class Max {
     public static int max(int first, int second) {
         return first > second ? first : second;
     }
+
+    public static int max(int first, int second, int third) {
+        return max(first, max(second, third));
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return max(max(first, second), max(third, fourth));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(2, 32, 44, 56));
+    }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -7,14 +7,20 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest {
     @Test
-    public void whenMax2To3Then3() {
+    public void whenMax3() {
         int result = Max.max(2, 3);
         assertThat(result, is(3));
     }
 
     @Test
-    public void whenMax3To2Then3() {
-        int result = Max.max(3, 2);
-        assertThat(result, is(3));
+    public void whenMax4() {
+        int result = Max.max(3, 2, 4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMax5() {
+        int result = Max.max(2, 3, 4, 5);
+        assertThat(result, is(5));
     }
 }
