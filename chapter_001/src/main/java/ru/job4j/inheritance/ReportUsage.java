a314bf9dd084c4c73c5ea58c1cb9e6a1abe43952
avtokot <avtokot84@gmail.com>
2020-04-15 12:00:09 +0300

    Added @override

--- chapter_001/src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ chapter_001/src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -2,8 +2,6 @@ package ru.job4j.inheritance;
 
 public class ReportUsage {
     public static void main(String[] args) {
-//        TextReport textReport = new TextReport();
-//        HtmlReport htmlReport = new HtmlReport();
         JSONReport jsonReport = new JSONReport();
         String text = jsonReport.generate("name", "body");
         System.out.println(text);
