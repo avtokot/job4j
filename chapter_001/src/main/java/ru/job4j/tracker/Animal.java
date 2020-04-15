2a18402d0f77dd5f3d596f4b627c5f11084120cc
avtokot <avtokot84@gmail.com>
2020-04-15 12:19:45 +0300

    Fix Constructor overload

--- chapter_001/src/main/java/ru/job4j/tracker/Animal.java
+++ chapter_001/src/main/java/ru/job4j/tracker/Animal.java
@@ -3,15 +3,10 @@ package ru.job4j.tracker;
 public class Animal {
     private String name;
 
+    public Animal() {
+
+    }
     public Animal(String name) {
         this.name = name;
-    }
-
-    public Animal() {
-        System.out.println("Animal");
-    }
-
-    public static void main(String[] args) {
-        Animal animal = new Animal();
     }
 }
--- chapter_001/src/main/java/ru/job4j/tracker/Tiger.java
+++ chapter_001/src/main/java/ru/job4j/tracker/Tiger.java
@@ -1,13 +1,7 @@
 package ru.job4j.tracker;
 
 public class Tiger extends Animal {
-
     public Tiger() {
         super();
-        System.out.println("Tiger");
-    }
-
-    public static void main(String[] args) {
-        Tiger tiger = new Tiger();
     }
 }
