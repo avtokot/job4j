5dbf13d8387320b1e83394e4a914b58c24c1e9ad
avtokot <avtokot84@gmail.com>
2020-04-28 17:38:18 +0300

    Up casting

--- chapter_001/src/main/java/ru/job4j/oop/Student.java
+++ chapter_001/src/main/java/ru/job4j/oop/Student.java
@@ -1,19 +1,4 @@
 package ru.job4j.oop;
 
-public class Student {
-    public static void main(String[] args) {
-        Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
-        petya.sound();
-        petya.sound();
-    }
-
-    public void music() {
-        System.out.println("Tra - tra - tra");
-    }
-    public void sound() {
-        System.out.println("I believe i can fly");
-    }
+public class Student extends Object {
 }
