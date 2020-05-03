914fb1837b29801d32bac7c0b32c845f8e211c85
avtokot <avtokot84@gmail.com>
2020-05-03 22:45:22 +0300

    Fix Chapter_2 Pom

--- chapter_001/src/test/java/ru/job4j/StartUiTest.java
+++ chapter_001/src/test/java/ru/job4j/StartUiTest.java
@@ -1,6 +1,7 @@
-package ru.job4j.tracker;
+package ru.job4j;
 
 import org.junit.Test;
+import ru.job4j.tracker.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
