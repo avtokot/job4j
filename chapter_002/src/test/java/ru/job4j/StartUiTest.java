476843f0010dfc7c9879c8f5dfdce8f042489075
avtokot <avtokot84@gmail.com>
2020-05-06 22:32:30 +0300

    StartUiTest deleteItem

--- chapter_002/src/test/java/ru/job4j/StartUiTest.java
+++ chapter_002/src/test/java/ru/job4j/StartUiTest.java
@@ -1,8 +1,10 @@
 package ru.job4j;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.tracker.*;
 
+import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -16,5 +18,27 @@ public class StartUiTest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new Item");
+        tracker.add(item);
+        String[] answers = {item.getId(), "replaced item"};
+        StartUi.editItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new Item");
+        tracker.add(item);
+        String[] answers = {item.getId()};
+        StartUi.delItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertThat(deleted, is(nullValue()));
     }
 }
