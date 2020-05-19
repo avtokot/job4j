2e7dbd2e811c427df295bd7080ecc56cd200adef
avtokot <avtokot84@gmail.com>
2020-05-19 11:43:38 +0300

    Interface UserAction

--- chapter_002/src/main/java/ru/job4j/tracker/StartUi.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUi.java
@@ -2,107 +2,157 @@ package ru.job4j.tracker;
 
 
 public class StartUi {
-    public void init(Input input, Tracker tracker) {
+    public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
-            this.showMenu();
+            this.showMenu(actions);
             int select = input.askInt("Select : ");
-            if (select == 1) {
-                StartUi.add(input, tracker);
-            } else if (select == 2) {
-                StartUi.showAllItems(tracker);
-            } else if (select == 3) {
-                StartUi.editItem(input, tracker);
-            } else if (select == 4) {
-                StartUi.delItem(input, tracker);
-            } else if (select == 5) {
-                StartUi.findId(input, tracker);
-            } else if (select == 6) {
-                StartUi.findName(input, tracker);
-            } else if (select == 7) {
-                System.out.println("==== Exit ====");
+            if (select == 6) {
+                System.out.println("Exit");
                 run = false;
+            } else {
+                UserAction action = actions[select];
+                run = action.execute(input, tracker);
             }
         }
     }
 
-    public static void editItem(Input input, Tracker tracker) {
-        System.out.println("==== Edit item ====");
-        String id = input.askStr("Enter id: ");
-        String name = input.askStr("Enter new name: ");
-        Item item = new Item(name);
-        if (tracker.replaceItem(id, item)) {
-            System.out.println("Request completed");
-        } else {
-            System.out.println("Application not found");
+    public static class EditAction implements UserAction {
+
+        @Override
+        public String name() {
+            return "Edit item";
         }
-    }
 
-
-    public static void delItem(Input input, Tracker tracker) {
-        System.out.println("==== Delete item ====");
-        String id = input.askStr("Enter id: ");
-        if (tracker.deleteItem(id)) {
-            System.out.println("Request completed");
-        } else {
-            System.out.println("Application not found");
-        }
-    }
-
-    public static void add(Input input, Tracker tracker) {
-        System.out.println("==== Create new item ====");
-        String name = input.askStr("Enter name: ");
-        Item item = new Item(name);
-        tracker.add(item);
-    }
-
-    public static void findName(Input input, Tracker tracker) {
-        System.out.println("==== Find item by name ====");
-        String name = input.askStr("Enter name: ");
-        Item[] names = tracker.findByName(name);
-        if (names.length != 0) {
-            for (int i = 0; i < names.length; i++) {
-                System.out.println(names[i]);
+        @Override
+        public boolean execute(Input input, Tracker tracker) {
+            String id = input.askStr("Enter id: ");
+            String name = input.askStr("Enter new name: ");
+            Item item = new Item(name);
+            if (tracker.replaceItem(id, item)) {
+                System.out.println("Request completed");
+            } else {
+                System.out.println("Application not found");
             }
-        } else {
-            System.out.println("Sorry, item not found :(");
-        }
-
-    }
-
-    public static void findId(Input input, Tracker tracker) {
-        System.out.println("==== Find by id ====");
-        String id = input.askStr("Enter id: ");
-        Item result = tracker.findById(id);
-        if (result != null) {
-            System.out.println(result.getName());
-        } else {
-            System.out.println("Item not found :(");
+            return true;
         }
     }
 
-    public static void showAllItems(Tracker tracker) {
-        System.out.println("==== Show all items ====");
-        Item[] items = tracker.findAll();
-        for (int i = 0; i < items.length; i++) {
-            System.out.println(items[i]);
+
+    public static class DeleteAction implements UserAction {
+
+        @Override
+        public String name() {
+            return "Delete item";
+        }
+
+        @Override
+        public boolean execute(Input input, Tracker tracker) {
+            String id = input.askStr("Enter id: ");
+            if (tracker.deleteItem(id)) {
+                System.out.println("Request completed");
+            } else {
+                System.out.println("Application not found");
+            }
+            return true;
         }
     }
 
-    public void showMenu() {
+    public static class CreateAction implements UserAction {
+
+        @Override
+        public String name() {
+            return "Create new item";
+        }
+
+        @Override
+        public boolean execute(Input input, Tracker tracker) {
+            String name = input.askStr("Enter name: ");
+            Item item = new Item(name);
+            tracker.add(item);
+            return true;
+        }
+    }
+
+
+    public static class FindByNameAction implements UserAction {
+
+        @Override
+        public String name() {
+            return "Find item by name";
+        }
+
+        @Override
+        public boolean execute(Input input, Tracker tracker) {
+            String name = input.askStr("Enter name: ");
+            Item[] names = tracker.findByName(name);
+            if (names.length != 0) {
+                for (int i = 0; i < names.length; i++) {
+                    System.out.println(names[i]);
+                }
+            } else {
+                System.out.println("Sorry, item not found :(");
+            }
+            return true;
+        }
+    }
+
+
+    public static class FindIdAction implements UserAction {
+
+        @Override
+        public String name() {
+            return "Find by id";
+        }
+
+        @Override
+        public boolean execute(Input input, Tracker tracker) {
+            String id = input.askStr("Enter id: ");
+            Item result = tracker.findById(id);
+            if (result != null) {
+                System.out.println(result.getName());
+            } else {
+                System.out.println("Item not found :(");
+            }
+            return true;
+        }
+    }
+
+    public static class ShowAllAction implements UserAction {
+
+        @Override
+        public String name() {
+            return "Show all items";
+        }
+
+        @Override
+        public boolean execute(Input input, Tracker tracker) {
+            Item[] items = tracker.findAll();
+            for (int i = 0; i < items.length; i++) {
+                System.out.println(items[i]);
+            }
+            return true;
+        }
+    }
+
+    public void showMenu(UserAction[] actions) {
         System.out.println("Menu");
-        System.out.println("1. Add new item");
-        System.out.println("2. Show all items");
-        System.out.println("3. Edit item");
-        System.out.println("4. Delete item");
-        System.out.println("5. Find item by id");
-        System.out.println("6. Find item by name");
-        System.out.println("7. Exit");
+        for (int i = 0; i < actions.length; i++) {
+            System.out.println(i + ". " + actions[i].name());
+        }
     }
 
     public static void main(String[] args) {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
-        new StartUi().init(input, tracker);
+        UserAction[] actions = {
+                new CreateAction(),
+                new ShowAllAction(),
+                new EditAction(),
+                new DeleteAction(),
+                new FindIdAction(),
+                new FindByNameAction()
+        };
+        new StartUi().init(input, tracker, actions);
     }
 }
--- chapter_002/src/test/java/ru/job4j/StartUiTest.java
+++ chapter_002/src/test/java/ru/job4j/StartUiTest.java
@@ -14,7 +14,7 @@ public class StartUiTest {
         String[] answers = {"Fix PC"};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
-        StartUi.add(input, tracker);
+//        StartUi.add(input, tracker);
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
@@ -26,7 +26,7 @@ public class StartUiTest {
         Item item = new Item("new Item");
         tracker.add(item);
         String[] answers = {item.getId(), "replaced item"};
-        StartUi.editItem(new StubInput(answers), tracker);
+//        StartUi.editItem(new StubInput(answers), tracker);
         Item replaced = tracker.findById(item.getId());
         assertThat(replaced.getName(), is("replaced item"));
     }
@@ -37,7 +37,7 @@ public class StartUiTest {
         Item item = new Item("new Item");
         tracker.add(item);
         String[] answers = {item.getId()};
-        StartUi.delItem(new StubInput(answers), tracker);
+//        StartUi.delItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
         assertThat(deleted, is(nullValue()));
     }
