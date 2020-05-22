cb3e03d891e24aaa5b1ee720c9e73ef947834127
avtokot <avtokot84@gmail.com>
2020-05-22 21:40:27 +0300

    Before and After testing

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -10,11 +12,21 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+    PrintStream printStream = System.out;
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(printStream);
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream printStream = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(new String(out.toByteArray()), is(new StringJoiner(System.lineSeparator())
                 .add("+++++")
@@ -23,13 +35,10 @@ public class PaintTest {
                 .add("+++++\r\n")
                 .toString()
         ));
-        System.setOut(printStream);
     }
+
     @Test
     public void whenDrawTriangle() {
-        PrintStream printStream = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(new String(out.toByteArray()), is(new StringJoiner(System.lineSeparator())
                 .add("+")
@@ -38,6 +47,5 @@ public class PaintTest {
                 .add("++++\r\n")
                 .toString()
         ));
-        System.setOut(printStream);
     }
 }
