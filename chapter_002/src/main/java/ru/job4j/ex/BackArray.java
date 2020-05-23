6d58363acb2b0d7435a3c04ba7b2d43d598cd32d
avtokot <avtokot84@gmail.com>
2020-05-23 16:59:42 +0300

    Fix exception

--- chapter_002/src/main/java/ru/job4j/ex/BackArray.java
+++ chapter_002/src/main/java/ru/job4j/ex/BackArray.java
@@ -1,4 +1,4 @@
-package ru.job4j.ru.job4j.ex;
+package ru.job4j.ex;
 
 import java.util.Arrays;
 
@@ -6,10 +6,10 @@ public class BackArray {
     public static void main(String[] args) {
         String[] names = {"Gillan", "Glover", "Blackmore", "Lord"};
         int middle = names.length / 2;
-        for (int i = 0; i < names.length; i++) {
+        for (int i = 0; i < middle; i++) {
             String temp = names[i];
-            names[i] = names[names.length - 1];
-            names[names.length - 1] = temp;
+            names[i] = names[names.length - i - 1];
+            names[names.length - i - 1] = temp;
         }
         System.out.println(Arrays.toString(names));
     }
