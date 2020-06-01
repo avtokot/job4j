f8300dcdff5c711117388986adb52eee784634da
avtokot <avtokot84@gmail.com>
2020-06-01 16:01:42 +0300

    Fix ValidateInputTest

--- chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -9,14 +11,30 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class ValidateInputTest {
+    ByteArrayOutputStream in = new ByteArrayOutputStream();
+    PrintStream out = System.out;
+
+    @Before
+    public void loadInput() {
+        System.setOut(new PrintStream(in));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(out);
+    }
+
     @Test
     public void whenInvalidInput() {
-        ByteArrayOutputStream in = new ByteArrayOutputStream();
-        PrintStream out = System.out;
-        System.setOut(new PrintStream(in));
         ValidateInput input = new ValidateInput(new StubInput(new String[]{"one", "1"}));
-        input.askInt("Enter");
+        input.askInt("Invalid");
         assertThat(in.toString(), is(String.format("Please enter validate data again%n")));
-        System.setOut(out);
+    }
+
+    @Test
+    public void whenNotInvalidInput() {
+        ValidateInput input = new ValidateInput(new StubInput(new String[]{"7", "5"}));
+        input.askInt("Select : ", 7);
+        assertThat(in.toString(), is(String.format("Please select key from menu%n")));
     }
 }
