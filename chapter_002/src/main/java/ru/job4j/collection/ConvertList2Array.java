dc8c807b68352180a17b5ffd8a14973c4cd1fd5b
avtokot <avtokot84@gmail.com>
2020-11-07 22:01:47 +0200

    ArrayList to 2d arr

---
+++ chapter_002/src/main/java/ru/job4j/collection/ConvertList2Array.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+import java.util.List;
+
+public class ConvertList2Array {
+    public int[][] toArray(List<Integer> list, int cells) {
+        int groups = (int) Math.ceil((double) list.size() / cells);
+        int[][] array = new int[groups][cells];
+        int row = 0, cell = 0;
+        for (Integer num : list) {
+            if (cell == cells) {
+              cell = 0;
+                row++;
+            }
+            array[row][cell++] = num;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7);
+        ConvertList2Array convert = new ConvertList2Array();
+        int[][] result = convert.toArray(list, 3);
+        for (int[] row : result) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
