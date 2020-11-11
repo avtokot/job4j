b31bb69caa1d61c3ed071e5fe06dff5a1bb68eae
avtokot <avtokot84@gmail.com>
2020-11-11 21:39:33 +0200

    ConvertMatrix2List

---
+++ chapter_002/src/test/java/ru/job4j/collection/ConvertMatrix2ListTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConvertMatrix2ListTest {
+    @Test
+    public void whenConvertMatrixTo2List() {
+        ConvertMatrix2List convert = new ConvertMatrix2List();
+        int[][] array = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(1, 2, 3, 4);
+        List<Integer> result = convert.toList(array);
+        assertThat(result, is(expect));
+    }
+}
