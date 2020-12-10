c30527ff40898c52126eb70b070b623ec94a96b7
avtokot <avtokot84@gmail.com>
2020-12-10 12:46:08 +0200

    Fix @Annotations

--- chapter_002/src/test/java/ru/job4j/tracker/StartUiTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUiTest.java
@@ -2,9 +2,9 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.IsNull.nullValue;
 
 
 public class StartUiTest {
