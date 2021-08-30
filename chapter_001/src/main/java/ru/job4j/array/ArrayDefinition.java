4c60841a4eb5a694476af71e389449a64e32ecba
avtokot <avtokot84@gmail.com>
2021-08-30 22:44:21 +0300

    Fill_The_Array

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива " + surname.length);
         float[] prices = new float[40];
         System.out.println("Размер массива " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Kot";
+        names[1] = "Glover";
+        names[2] = "Gillan";
+        names[3] = "Lord";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
