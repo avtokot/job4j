f62fd519bdf401d3be95e6e55180670efa7b9c03
avtokot <avtokot84@gmail.com>
2022-01-11 23:06:29 +0200

    Fix ShopDrop

--- chapter_001/src/main/java/ru/job4j/pojo/ShopDrop.java
+++ chapter_001/src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -1,6 +1,8 @@
 package ru.job4j.pojo;
 
-
+/**
+ * ShopDrop.
+ */
 public class ShopDrop {
     public static Book[] delete(Book[] books, int index) {
         for (int i = index; i < books.length - 1; i++) {
