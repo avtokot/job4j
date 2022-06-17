9e2766a6dacabb307515f7548cedbbe267456829
avtokot <avtokot84@gmail.com>
2022-06-17 22:39:51 +0300

    for-each

--- chapter_001/src/main/java/ru/job4j/array/ArrayLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayLoop.java
@@ -1,23 +1,17 @@
 package ru.job4j.array;
 
-/**
- * ArrayLoop.
- */
-
 public class ArrayLoop {
-    /**
-     * @param args - someNumbers
-     */
-    public final void main(String[] args) {
+
+    public static void main(String[] args) {
         final int someNumbers = 5;
-        int[] numbers = new int[someNumbers];
         final int someNumbersTwo = 3;
+        int[] numbers = new int[someNumbers];
+
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i * 2 + someNumbersTwo;
         }
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+        for (int number : numbers) {
+            System.out.println(number);
         }
-
     }
 }
